5c43141e37b436748ecf1610a1168a020c412fcb
mohangav <mohangavande@outlook.com>
2022-08-14 19:30:39 +0200

    Exercise final commit

--- example13a/src/main/java/com/example/main/Example13a.java
+++ example13a/src/main/java/com/example/main/Example13a.java
@@ -1,17 +1,28 @@
 package com.example.main;
 
 import com.example.beans.Person;
+import com.example.beans.Vehicle;
 import com.example.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Example13a {
 
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+      /*  var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         Person person = context.getBean(Person.class);
         System.out.println("Person name from Spring Context is: " + person.getName());
         System.out.println("Vehicle that Person own is: " + person.getVehicle().getName());
         System.out.println("Tyre that Vehicle use is: " + person.getVehicle().getVehicleService().rotate());
         System.out.println("Sound that Vehicle play is: " + person.getVehicle().getVehicleService().makeSound());
+*/
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        String[] persons = context.getBeanNamesForType(Person.class);
+        Person person = context.getBean(Person.class);
+        String[] names = context.getBeanNamesForType(Vehicle.class);
+        /*Vehicle vehicle = context.getBean(Vehicle.class);
+        vehicle.getVehicleServices().playMusic();
+        vehicle.getVehicleServices().moveVehicle();*/
+        person.getVehicle().getVehicleService().makeSound() ;
+        person.getVehicle().getVehicleService().rotate();
     }
 }
