8791576bd4f30c5338748991dedce32d293ddf41
mohangav <mohangavande@outlook.com>
2022-08-14 17:33:49 +0200

    Circular Dependency example 12a

---
+++ example12_1/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,29 @@
+package com.example.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+//@Component
+public class Vehicle {
+
+    private String name = "cooper";
+
+    @Autowired
+    private Person person;
+
+    public Vehicle() {
+        System.out.println("Vehicle Instance initialition");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+ }
